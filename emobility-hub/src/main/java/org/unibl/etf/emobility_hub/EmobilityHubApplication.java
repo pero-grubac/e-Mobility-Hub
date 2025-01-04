f5064b7c8b0a32f452bcd7e612bf21d40d157445
Pero <perogrubac99@gmail.com>
2025-01-04 17:21:37 +0100

    setup of the backend part

---
+++ emobility-hub/src/main/java/org/unibl/etf/emobility_hub/EmobilityHubApplication.java
@@ -0,0 +1,13 @@
+package org.unibl.etf.emobility_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmobilityHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmobilityHubApplication.class, args);
+	}
+
+}
