d3b72d7866b192de8b1cb112ce052e95bb3e1017
Pero <perogrubac99@gmail.com>
2025-01-05 21:05:12 +0100

    EmployeeEntity

---
+++ emobility-hub/src/main/java/org/unibl/etf/emobility_hub/models/entity/EmployeeEntity.java
@@ -0,0 +1,16 @@
+package org.unibl.etf.emobility_hub.models.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@Entity
+@Table(name="employee")
+public class EmployeeEntity extends UserEntity {
+    @Column(nullable = false)
+    private String role;
+}
