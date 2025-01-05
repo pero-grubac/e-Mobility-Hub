1e2847d8cc80436a74feed33348a964b02208c2f
Pero <perogrubac99@gmail.com>
2025-01-05 21:36:49 +0100

    ElectricCarEntityRepository

---
+++ emobility-hub/src/main/java/org/unibl/etf/emobility_hub/repositories/ElectricCarEntityRepository.java
@@ -0,0 +1,9 @@
+package org.unibl.etf.emobility_hub.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import org.unibl.etf.emobility_hub.models.entity.ElectricCarEntity;
+
+@Repository
+public interface ElectricCarEntityRepository extends CrudRepository<ElectricCarEntity, Long> {
+}
