959ce775c16c5e04bb7d45f8e72271adb219550d
Pero <perogrubac99@gmail.com>
2025-01-26 20:43:24 +0100

    welcome page

--- emobility-hub-employee/src/app/app.component.ts
+++ emobility-hub-employee/src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'emobility-hub-employee';
+  isHomePage: boolean = false;
+  
+  constructor(private router: Router) {
+    this.router.events.subscribe(() => {
+      this.isHomePage = this.router.url === '/';
+    });
+  }
 }
