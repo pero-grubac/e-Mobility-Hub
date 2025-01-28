dcf236470f6c135fd0cb27438fa569d983a95d14
Pero <perogrubac99@gmail.com>
2025-01-28 20:49:07 +0100

    employee item

--- emobility-hub-employee/src/app/components/employee/employee-item/employee-item.component.ts
+++ emobility-hub-employee/src/app/components/employee/employee-item/employee-item.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { User } from '../../../models/user.model';
 
 @Component({
   selector: 'app-employee-item',
   templateUrl: './employee-item.component.html',
-  styleUrl: './employee-item.component.css'
+  styleUrl: './employee-item.component.css',
 })
 export class EmployeeItemComponent {
+  @Input() employee!: User;
+  @Output() employeeClick = new EventEmitter<number>();
 
+  onEmployeeClick(): void {
+    this.employeeClick.emit(this.employee.id);
+  }
+
+  getRoleImage(role: string): string {
+    switch (role) {
+      case 'ROLE_ADMIN':
+        return 'assets/images/admin.png';
+      case 'ROLE_OPERATOR':
+        return 'assets/images/operator.png';
+      case 'ROLE_MANAGER':
+        return 'assets/images/manager.png';
+      default:
+        return 'assets/images/default-avatar.png'; 
+    }
+  }
+
+  formatRole(role: string): string {
+    switch (role) {
+      case 'ROLE_ADMIN':
+        return 'Administrator';
+      case 'ROLE_OPERATOR':
+        return 'Operator';
+      case 'ROLE_MANAGER':
+        return 'Manager';
+      default:
+        return 'Unknown Role';
+    }
+  }
 }
