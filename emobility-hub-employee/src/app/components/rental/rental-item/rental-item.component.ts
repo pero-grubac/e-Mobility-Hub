aba252d97d48a82db607386404c6862bcab419f8
Pero <perogrubac99@gmail.com>
2025-02-01 19:01:18 +0100

    rental item

--- emobility-hub-employee/src/app/components/rental/rental-item/rental-item.component.ts
+++ emobility-hub-employee/src/app/components/rental/rental-item/rental-item.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Rental } from '../../../models/rental.model';
 
 @Component({
   selector: 'app-rental-item',
   templateUrl: './rental-item.component.html',
-  styleUrl: './rental-item.component.css'
+  styleUrl: './rental-item.component.css',
 })
-export class RentalItemComponent {
+export class RentalItemComponent implements OnInit {
+  @Input() rental!: Rental;
+  @Output() rentalClick = new EventEmitter<number>();
 
+  ngOnInit(): void {}
+
+  onRentalClick(): void {
+    this.rentalClick.emit(this.rental.id); 
+  }
 }
