2a299f30dafb18f61a3cbfc358656120bb470ac2
Pero <perogrubac99@gmail.com>
2025-02-01 18:52:36 +0100

    rental service and methodes getById,getAllByClientId,getAllByVehicleId

--- emobility-hub-employee/src/app/models/rental.model.ts
+++ emobility-hub-employee/src/app/models/rental.model.ts
@@ -1,5 +1,7 @@
+import { Client } from './client.models';
 import { Location } from './location.model';
 import { PageMetadata } from './pageMetadata.model';
+import { Vehicle } from './vehicle.model';
 
 export interface Rental {
   id: number;
@@ -15,4 +17,9 @@ export interface Rental {
 export interface RentalPage {
   content: Rental[];
   page: PageMetadata;
+}
+
+export interface DetiledRental extends Rental {
+  client: Client;
+  vehicle: Vehicle;
 }
--- emobility-hub-employee/src/app/services/rental.service.ts
+++ emobility-hub-employee/src/app/services/rental.service.ts
@@ -1,6 +1,8 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../config/environment';
+import { Observable } from 'rxjs';
+import { DetiledRental, RentalPage } from '../models/rental.model';
 
 @Injectable({
   providedIn: 'root',
@@ -9,4 +11,38 @@ export class RentalService {
   constructor(private http: HttpClient) {}
 
   baseUrl: string = `${environment.baseUrl}${environment.rentals}`;
+  getAllByVehicleId(
+    page: number,
+    size: number,
+    id: number
+  ): Observable<RentalPage> {
+    let params = new HttpParams()
+      .set('page', page.toString())
+      .set('size', size.toString())
+      .set('sort', 'id,desc');
+    return this.http.get<RentalPage>(
+      `${this.baseUrl}/getAllByVehicleId/${id}`,
+      {
+        params,
+      }
+    );
+  }
+
+  getAllByClientId(
+    page: number,
+    size: number,
+    id: number
+  ): Observable<RentalPage> {
+    let params = new HttpParams()
+      .set('page', page.toString())
+      .set('size', size.toString())
+      .set('sort', 'id,desc');
+    return this.http.get<RentalPage>(`${this.baseUrl}/getAllByClientId/${id}`, {
+      params,
+    });
+  }
+  
+  getById(id: number): Observable<DetiledRental> {
+    return this.http.get<DetiledRental>(`${this.baseUrl}/${id}`);
+  }
 }
