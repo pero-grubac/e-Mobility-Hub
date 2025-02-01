a083e33d00fc79b731460886f1723b32f663eb86
Pero <perogrubac99@gmail.com>
2025-02-01 19:54:26 +0100

    statistics service

--- emobility-hub-employee/src/app/services/statistic.service.ts
+++ emobility-hub-employee/src/app/services/statistic.service.ts
@@ -1,6 +1,12 @@
-import { HttpClient } from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { environment } from "../config/environment";
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from '../config/environment';
+import { Observable } from 'rxjs';
+import {
+  DailyRevenue,
+  FaultsPerVehicle,
+  RevenueByVehicleType,
+} from '../models/statistic.models';
 
 @Injectable({
   providedIn: 'root',
@@ -9,4 +15,23 @@ export class StatisticService {
   constructor(private http: HttpClient) {}
 
   baseUrl: string = `${environment.baseUrl}${environment.statistics}`;
+
+  getDailyRevenue(start: string, end: string): Observable<DailyRevenue[]> {
+    let params = new HttpParams()
+      .set('start', start.toString())
+      .set('end', end.toString());
+    return this.http.get<DailyRevenue[]>(`${this.baseUrl}/getDailyRevenue`, {
+      params,
+    });
+  }
+  getRevenueByVehicleType(): Observable<RevenueByVehicleType[]> {
+    return this.http.get<RevenueByVehicleType[]>(
+      `${this.baseUrl}/getRevenueByVehicleType`
+    );
+  }
+  getFaultsPerVehicle(): Observable<FaultsPerVehicle[]> {
+    return this.http.get<FaultsPerVehicle[]>(
+      `${this.baseUrl}/getFaultsPerVehicle`
+    );
+  }
 }
